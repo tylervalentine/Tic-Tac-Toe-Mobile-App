cb37d9ab7fbfef4df6f63faf7ea190eeb83afab3
Tyler Valentine <valentinet02@moravian.edu>
2022-10-07 17:09:51 -0400

    Complete Game and Welcome Activity

--- app/src/main/java/edu/moravian/csci215/tictactoe/WelcomeActivity.kt
+++ app/src/main/java/edu/moravian/csci215/tictactoe/WelcomeActivity.kt
@@ -9,15 +9,13 @@ import edu.moravian.csci215.tictactoe.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity: AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
-    private var spinnerOptions = listOf("Human", "Easy", "Medium", "Hard")
-
     private lateinit var binding: ActivityWelcomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSpinnerOptions(spinnerOptions)
+
         binding.spinner.onItemSelectedListener = this
         binding.spinner2.onItemSelectedListener = this
 
@@ -25,38 +23,32 @@ class WelcomeActivity: AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     fun changeScreenView(view: View?) {
         val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-    }
 
-    private fun setSpinnerOptions(optionsList: List<String>) {
-        val dataAdapter = ArrayAdapter(this, R.layout.spinner_style, optionsList)
-        dataAdapter.setDropDownViewResource(R.layout.spinner_dropdown_style)
-        binding.spinner.adapter = dataAdapter
-        binding.spinner2.adapter = dataAdapter
+        intent.putExtra("player_1_name", binding.nameText1.text.toString())
+        intent.putExtra("player_2_name", binding.nameText2.text.toString())
+        intent.putExtra("player_1_type", binding.spinner.selectedItemPosition)
+        intent.putExtra("player_2_type", binding.spinner2.selectedItemPosition)
+        startActivity(intent)
     }
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
         if(parent?.id == R.id.spinner) {
             if(parent.getItemAtPosition(pos).toString() == "Human") {
-                binding.name1.visibility = View.VISIBLE
                 binding.nameText1.visibility = View.VISIBLE
             }
             else
             {
-                binding.name1.visibility = View.GONE
-                binding.nameText1.visibility = View.GONE
+                binding.nameText1.visibility = View.INVISIBLE
             }
         }
 
         if(parent?.id == R.id.spinner2) {
             if(parent.getItemAtPosition(pos).toString() == "Human") {
-                binding.name2.visibility = View.VISIBLE
                 binding.nameText2.visibility = View.VISIBLE
             }
             else
             {
-                binding.name2.visibility = View.GONE
-                binding.nameText2.visibility = View.GONE
+                binding.nameText2.visibility = View.INVISIBLE
             }
         }
 
