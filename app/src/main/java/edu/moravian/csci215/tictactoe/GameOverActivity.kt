42588eec82e815c6a4c52c4402600f22fbdfce64
student <olsakowskid@moravian.edu>
2022-10-07 20:08:33 -0400

    horizontal layouts, icons, gameover start

---
+++ app/src/main/java/edu/moravian/csci215/tictactoe/GameOverActivity.kt
@@ -0,0 +1,24 @@
+package edu.moravian.csci215.tictactoe
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import edu.moravian.csci215.tictactoe.databinding.ActivityGameoverBinding
+
+
+class GameOverActivity : AppCompatActivity() {
+    //private lateinit var displayView2: TextView
+    private lateinit var binding: ActivityGameoverBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGameoverBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+    fun changeScreenView(view: View?) {
+        val intent = Intent(this, WelcomeActivity::class.java)
+        startActivity(intent)
+    }
+}
--- app/src/main/java/edu/moravian/csci215/tictactoe/MainActivity.kt
+++ app/src/main/java/edu/moravian/csci215/tictactoe/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.moravian.csci215.tictactoe
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -7,6 +8,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.reflect.typeOf
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,9 +29,9 @@ class MainActivity : AppCompatActivity() {
         3 to Pair(1, 0), 4 to Pair(1, 1), 5 to Pair(1, 2), 6 to Pair(2, 0),
         7 to Pair(2, 1), 8 to Pair(2, 2))
 
-    /** Variables for two test human players. */
-    private val player1 = HumanPlayer("Tyler")
-    private val player2 = HumanPlayer("Reed")
+    /** Variables for two players. */
+    private lateinit var player1: Player
+    private lateinit var player2: Player
 
     /** Variable for tic-tac-toe game. */
     private val game = Game()
@@ -40,23 +42,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setPlayers()
         game.startGame(player1, player2)
         game.startNewRound()
 
-
         game.onRoundOverListener = {
-            result -> showResultMessage(result)
+                result -> showResultMessage(result)
         }
 
         displayView = findViewById(R.id.display_text_view)
-        displayView.text = resources.getString(R.string.display_text, game.players[game.turn].name,
-            game.pieces[game.turn])
 
         for(index in buttonIds.indices)
         {
             findViewById<Button>(buttonIds[index]).setOnClickListener {
-                setButtonView(index)
+                playTurn(index)
             }
+        }
+
+        updateBoard()
+    }
+
+    private fun updateBoard()
+    {
+        for(index in buttonIds.indices) {
+            val button = findViewById<Button>(buttonIds[index])
+            button.text = game.board[index/3, index%3].toString()
+        }
+        if (game.turn != -1) {
+            displayView.text = getString(
+                R.string.display_text, game.players[game.turn].name,
+                game.pieces[game.turn]
+            )
         }
     }
 
@@ -64,33 +80,35 @@ class MainActivity : AppCompatActivity() {
      * Set the button text view specified in num.
      * @param num number of button to update on layout.
      */
-    private fun setButtonView(num: Int)
-    {
-        if (!game.roundInProgress)
-        {
+    private fun playTurn(index: Int) {
+        if (!game.roundInProgress) {
             showErrorMessage()
-        }
-        else {
-            val currentPiece = game.pieces[game.turn].toString()
+        } else {
             val playResult =
-                game.playPiece(boardLocations[num]!!.first, boardLocations[num]!!.second)
+                game.playPiece(boardLocations[index]!!.first, boardLocations[index]!!.second)
             if (!playResult)
                 showErrorMessage()
-            else {
-                if (game.turn == -1) {
-                    displayView.text = resources.getString(R.string.game_over_text)
-                    findViewById<Button>(buttonIds[num]).text = currentPiece
-                } else {
-                    findViewById<Button>(buttonIds[num]).text = currentPiece
-                    displayView.text = resources.getString(
-                        R.string.display_text,
-                        game.players[game.turn].name,
-                        game.pieces[game.turn]
-                    )
-                }
-            }
+        }
+        updateBoard()
+    }
+
+    private fun setPlayers() {
+        player1 = makePlayer(intent.getStringExtra("player_1_name") ?: "",
+            intent.getIntExtra("player_1_type", 0))
+
+        player2 = makePlayer(intent.getStringExtra("player_2_name") ?: "",
+            intent.getIntExtra("player_2_type", 0))
+    }
+
+    private fun makePlayer(name: String, type: Int): Player {
+        return when (type) {
+            0 -> HumanPlayer(name)
+            1 -> EasyAIPlayer()
+            2 -> MediumAIPlayer()
+            else -> HardAIPlayer()
         }
     }
+
 
     /**
      * Show toast when a player has attempted to put a piece in a spot that already contains one,
